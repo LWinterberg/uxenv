28da672c5c60b5f057017887486a1106d481640e
Johan Althoff <teetow@gmail.com>
2023-02-18 15:35:24 +0100

    chore: build fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    assetsDir: "./",
+  },
   plugins: [react(), vanillaExtractPlugin()],
   css: {
     postcss: {
