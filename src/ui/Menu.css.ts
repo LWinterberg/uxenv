54600d91f147d0a40763fa1eb28a5e5cecc7dae7
Johan Althoff <teetow@gmail.com>
2023-02-22 17:52:18 +0100

    fix: tweak Menu colors

--- src/ui/Menu.css.ts
+++ src/ui/Menu.css.ts
@@ -1,26 +1,25 @@
 import { createVar, style } from "@vanilla-extract/css";
 
-const bgColor = createVar();
+export const bgColor = createVar();
 
 export const menuitem = style({
   backgroundColor: bgColor,
   borderRadius: "2px",
-  fontSize: "14px",
-  padding: "0.1rem 0.5rem",
+  fontSize: "0.75rem",
+  fontWeight: "500",
+  padding: "0.35rem 0.5rem",
   position: "relative",
 
   selectors: {
     "&:is-active": {},
     "&:hover, &.is-active": {
-      backgroundColor: "#aaa",
+      backgroundColor: "#c4d5e4",
     },
-  },
-  vars: {
-    [bgColor]: "#F5F5F7",
   },
 });
 
 export const menu = style({
+  backgroundColor: bgColor,
   display: "grid",
   gridAutoFlow: "row",
   position: "relative",
@@ -48,13 +47,19 @@ export const menu__list = style({
   position: "absolute",
   top: 0,
   transition: "opacity 60ms ease-in-out",
-
+  // filter: "drop-shadow(0 0 3px 8px #0009)",
+  boxShadow: `
+  0 3px 36px 0 #0002, 
+  0 3px 8px 0 #0002,
+  0 0 0 1px #0002
+  `,
+  zIndex: 1,
   selectors: {
     [`& > ${menu} > &`]: {
       left: "100%",
       top: 0,
     },
-    
+
     [`${menuitem}:not(.is-active) &`]: {
       opacity: 0,
       display: "none",
--- src/ui/MenuBar.css.ts
+++ src/ui/MenuBar.css.ts
@@ -1,36 +1,27 @@
-import { createVar, style } from "@vanilla-extract/css";
-
-const bgColor = createVar();
+import { style } from "@vanilla-extract/css";
+import { bgColor } from "./Menu.css";
 
 export const menubar = style({
+  background: bgColor,
   display: "grid",
   gridAutoFlow: "column",
   userSelect: "none",
   justifyContent: "start",
 
   vars: {
-    [bgColor]: "#F5F5F7",
+    [bgColor]: "#f5f5f7",
   },
 });
 
 export const menubaritem = style({
-  backgroundColor: bgColor,
   borderRadius: "2px",
   fontSize: "14px",
   padding: "0.25rem 0.5rem",
   position: "relative",
-
-  selectors: {
-    "&:is-active": {},
-    "&:hover, &.is-active": {
-      backgroundColor: "#aaa",
-    },
-  },
 });
 
 export const menubarmenu = style({
   position: "absolute",
   top: "2rem",
   left: 0,
-  backgroundColor: bgColor,
 });
